58c023d07f8d4378a92de9282d4943e0582ccad6
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-08-20 20:54:47 -0300

    add loading and error states to the context

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -3,7 +3,11 @@ import { getUser } from '@/services'
 import { createContext, useContext, useEffect, useState } from 'react'
 
 type USerContextType = {
-  user: User | null
+  user: {
+    response: User | null
+    loading: boolean
+    error: Error | null
+  }
 }
 
 const userContext = createContext({} as USerContextType)
@@ -13,12 +17,30 @@ const useUserContext = () => useContext(userContext)
 const UserProvider: React.FC<{ children: React.ReactNode }> = ({
   children
 }) => {
-  const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<USerContextType['user']>({
+    response: null,
+    loading: true,
+    error: null
+  })
 
   useEffect(() => {
-    getUser().then(user => {
-      setUser(user)
-    })
+    getUser()
+      .then(user => {
+        // if the response is successful, set the user ✅
+        setUser({
+          loading: false,
+          error: null,
+          response: user
+        })
+      })
+      .catch(error => {
+        // if the response is unsuccessful, set the error ❌
+        setUser({
+          loading: false,
+          error,
+          response: null
+        })
+      })
   }, [])
 
   return (
