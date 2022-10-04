a0fd67fc157fb91627150aad82014bbbbe02f6d5
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-10-04 03:50:52 -0300

    refactoring hook deriving state

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,13 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export function usePagination<T> (initialArray: T[] = []) {
   const [page, setPage] = useState(1)
   const [limit, setLimit] = useState(16)
-  const [total, setTotal] = useState(initialArray.length)
   const [itemsToShow, setItemsToShow] = useState(initialArray)
-  const [maxPage, setMaxPage] = useState(1)
-  const [canNext, setCanNext] = useState(false)
-  const [canPrev, setCanPrev] = useState(false)
+
+  const total = useMemo(() => initialArray.length, [initialArray])
+  const maxPage = useMemo(() => Math.ceil(total / limit), [total, limit])
+  const canPrev = useMemo(() => page > 1, [page])
+  const canNext = useMemo(() => page < maxPage, [page, maxPage])
+
+  useEffect(() => {
+    setItemsToShow(initialArray.slice((page - 1) * limit, page * limit))
+  }, [initialArray, page, limit])
 
   function nextPage () {
     if (!canNext) return
@@ -19,29 +24,13 @@ export function usePagination<T> (initialArray: T[] = []) {
     setPage(page - 1)
   }
 
-  useEffect(() => {
-    // first we check if can go to next page  ➡️
-    if (page === maxPage) setCanNext(false)
-    else setCanNext(true)
-    // then we check if can go to prev page ⬅️
-    if (page === 1) setCanPrev(false)
-    else setCanPrev(true)
-    // and finally we set the items to show 🚀
-    setItemsToShow(initialArray.slice((page - 1) * limit, page * limit))
-  }, [page, initialArray, limit])
-
-  useEffect(() => {
-    setTotal(initialArray.length)
-    setMaxPage(Math.ceil(initialArray.length / limit))
-  }, [initialArray])
-
   return {
     page,
     setPage,
     limit,
     setLimit,
     total,
-    setTotal,
+
     itemsToShow,
     nextPage,
     prevPage,
