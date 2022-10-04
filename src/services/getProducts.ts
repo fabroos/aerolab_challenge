5e7433bd3983db094b20f47fa6631fa0e8f076e3
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-10-04 01:26:23 -0300

    add get products services

--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -1,6 +1,7 @@
+import { Product } from '@/models'
 import headers from './headers'
 
-export const getProducts = () => {
+export const getProducts = (): Promise<Product[]> => {
   return fetch('https://coding-challenge-api.aerolab.co/products', {
     headers
   })
